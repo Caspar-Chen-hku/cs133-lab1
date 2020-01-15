de7ae775585a9a922c7ab848523ddf4a55851f57
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-15 15:01:39 -0600

    Update omp.cpp

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -5,7 +5,7 @@
 #include "gemm.h"
 
 // Using declarations, if any...
-
+/*
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
   // Your code goes here...
@@ -21,4 +21,23 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
       }
     }
   }
+}
+*/
+
+void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
+                  float c[kI][kJ]) {
+   int i,j,k;
+#pragma omp parallel shared(a,b,c) private(i,j,k) 
+   {
+  #pragma omp for schedule(static)
+    for (i=0; i< kI; i++){
+      std::memset(c[i], 0, sizeof(float) * kJ);
+        for (j=0; j< kJ; j++){
+          for (k=0; k< kK; k++){
+              c[i][j] += a[i][k] * b[k][j];
+          }
+        }
+    }
+   }
+   return 0;
 }
