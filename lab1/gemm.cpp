40a8f08585c7c4c0f221cc6b7a935f5801e844f7
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-15 14:31:29 -0600

    Update gemm.cpp

--- lab1/gemm.cpp
+++ lab1/gemm.cpp
@@ -6,6 +6,8 @@
 #include <random>
 #include <vector>
 
+#include <omp.h>
+
 #include "gemm.h"
 
 using std::chrono::duration_cast;
@@ -45,6 +47,22 @@ void GemmSequential(const float a[kI][kK], const float b[kK][kJ],
   }
   for (int i = 0; i < kI; ++i) {
     for (int j = 0; j < kJ; ++j) {
+      for (int k = 0; k < kK; ++k) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
+}
+
+void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
+                    float c[kI][kJ]) {
+  #pragma omp parallel for
+  for (int i = 0; i < kI; ++i) {
+    std::memset(c[i], 0, sizeof(float) * kJ);
+  }
+  for (int i = 0; i < kI; ++i) {
+    for (int j = 0; j < kJ; ++j) {
+      #pragma omp parallel for reduction (+:c[i][j])
       for (int k = 0; k < kK; ++k) {
         c[i][j] += a[i][k] * b[k][j];
       }
