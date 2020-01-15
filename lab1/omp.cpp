11dbb6d6d7406730ec78baf0cbe60aca5a5af101
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-15 15:27:01 -0600

    Update omp.cpp

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -32,8 +32,8 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
   #pragma omp for private(k,j) schedule(static)
     for (int i=0; i< kI; i++){
       std::memset(c[i], 0, sizeof(float) * kJ);
-        #pragma omp for private(j) schedule(static)
         for (k=0; k< kK; k++){
+          #pragma omp for schedule(static)
           for (j=0; j< kJ; j++){
               c[i][j] += a[i][k] * b[k][j];
           }
