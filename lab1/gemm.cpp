21b8edd55c941c9b7bed21e2636a6ba850e47e9f
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-15 14:49:16 -0600

    moved parallel from gemm to omp

--- lab1/gemm.cpp
+++ lab1/gemm.cpp
@@ -6,8 +6,6 @@
 #include <random>
 #include <vector>
 
-#include <omp.h>
-
 #include "gemm.h"
 
 using std::chrono::duration_cast;
@@ -54,21 +52,6 @@ void GemmSequential(const float a[kI][kK], const float b[kK][kJ],
   }
 }
 
-void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
-                    float c[kI][kJ]) {
-  #pragma omp parallel for
-  for (int i = 0; i < kI; ++i) {
-    std::memset(c[i], 0, sizeof(float) * kJ);
-  }
-  for (int i = 0; i < kI; ++i) {
-    for (int j = 0; j < kJ; ++j) {
-      #pragma omp parallel for reduction (+:c[i][j])
-      for (int k = 0; k < kK; ++k) {
-        c[i][j] += a[i][k] * b[k][j];
-      }
-    }
-  }
-}
 
 void Init(float a[kI][kK], float b[kK][kJ]) {
   clog << "Problem size: " << kI << " x " << kK << " x " <<  kJ << endl;
--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -1,4 +1,5 @@
 // Header inclusions, if any...
+#include <omp.h>
 
 #include "gemm.h"
 
@@ -7,4 +8,16 @@
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
   // Your code goes here...
+  #pragma omp parallel for
+  for (int i = 0; i < kI; ++i) {
+    std::memset(c[i], 0, sizeof(float) * kJ);
+  }
+  for (int i = 0; i < kI; ++i) {
+    for (int j = 0; j < kJ; ++j) {
+      #pragma omp parallel for reduction (+:c[i][j])
+      for (int k = 0; k < kK; ++k) {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
 }
