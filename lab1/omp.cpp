843902b097b43efd4b79bf75ecb48d03cb68817b
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-15 16:37:45 -0600

    Update omp.cpp

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -5,24 +5,6 @@
 #include "gemm.h"
 
 // Using declarations, if any...
-/*
-void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
-                  float c[kI][kJ]) {
-  // Your code goes here...
-  #pragma omp parallel for
-  for (int i = 0; i < kI; ++i) {
-    std::memset(c[i], 0, sizeof(float) * kJ);
-  }
-  for (int i = 0; i < kI; ++i) {
-    for (int j = 0; j < kJ; ++j) {
-      #pragma omp parallel for reduction (+:c[i][j])
-      for (int k = 0; k < kK; ++k) {
-        c[i][j] += a[i][k] * b[k][j];
-      }
-    }
-  }
-}
-*/
 
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
@@ -30,7 +12,6 @@ void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
   #pragma omp parallel for private(k,j)
     for (int i=0; i< kI; i++){
         std::memset(c[i], 0, sizeof(float) * kJ);
-        //#pragma omp parallel for private(j) schedule(static)
         for (k=0; k< kK; k++){
           for (j=0; j< kJ; j++){
               c[i][j] += a[i][k] * b[k][j];
