91d3c336f5360dd615783970863e18f58a79b852
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-16 14:15:34 -0600

    Update omp.cpp

--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -6,15 +6,30 @@
 
 // Using declarations, if any...
 
+void GemmTrans(const float b[kK][kJ], float bt[kJ][kK]){
+  int j;
+  #pragma omp parallel for private(j) schedule(static) num_threads(8)
+  for (int i = 0; i < kJ; i++)
+	{
+		for (j = 0; j < kK; j++)
+		{
+			bt[i][j] = b[j][i];
+		}
+	}
+}
+
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
                   float c[kI][kJ]) {
+    static float bt[kJ][kK];
+    GemmTrans(b,bt);
     int k,j,total;
   #pragma omp parallel for private(k,j) schedule(static) num_threads(8)
     for (int i=0; i< kI; i++){
         std::memset(c[i], 0, sizeof(float) * kJ);
-        for (k=0; k< kK; k++){
-          for (j=0; j< kJ; j++){
-              c[i][j] += a[i][k] * b[k][j];
+        for (j=0; j< kJ; j++){
+          for (k=0; k< kK; k++)
+          {
+              c[i][j] += a[i][k] * bt[j][k];
           }
         }
     }
