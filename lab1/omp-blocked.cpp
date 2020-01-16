2a6b0f800eb23df16f71b01a39e3ea7fcc5da4f0
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-16 14:27:28 -0600

    Update omp-blocked.cpp

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -9,7 +9,7 @@
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ]) {
   // Your code goes here...
-  int i,j,k0,j0,total;
+  int i,j,k0,j0;
   int BLOCK_SIZE = 256;
   /*
   #pragma omp parallel for private(k,j,i0,k0,j0) schedule(static) num_threads(8)
@@ -33,18 +33,15 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
    std::memset(c[i], 0, sizeof(float) * kJ);
  }
 
-  #pragma omp parallel for private(j,i,k0,j0,total) schedule(static) num_threads(8)
+  #pragma omp parallel for private(j,i,k0,j0) schedule(static) num_threads(8)
     for (int k=0; k< kK; k+=BLOCK_SIZE){
       for (j=0; j< kJ; j+=BLOCK_SIZE){
         for (i=0; i< kI; i++){
-          //std::memset(c[i], 0, sizeof(float) * kJ);
           for (k0=k; k0<k+BLOCK_SIZE; k0++){
             total = 0;
             for (j0=j; j0<j+BLOCK_SIZE; j0++){
-              //c[i][j0] += a[i][k0] * b[k0][j0];
-              total += a[i][k0] * b[k0][j0];
+              c[i][j0] += a[i][k0] * b[k0][j0];
             }
-            c[i][j0] += total;
           }
         }
     }
