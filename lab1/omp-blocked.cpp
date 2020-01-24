236a3f1c7cd5c45cb02fafb044483d3866089a1a
Caspar-Chen-hku <susume@connect.hku.hk>
2020-01-24 11:06:42 -0600

    Update omp-blocked.cpp

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -17,20 +17,18 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   int BLOCK_SIZE_J = 1024;
   int BLOCK_SIZE_K = 8;
 
-  int temp = 0;
-
   #pragma omp parallel for schedule(static) num_threads(8)
   for (int i = 0; i < kI; ++i) {
     std::memset(c[i], 0, sizeof(float) * kJ);
   }
 
-    #pragma omp parallel for private(temp) schedule(static) num_threads(8)
+    #pragma omp parallel for schedule(static) num_threads(8)
     for (int i=0; i< kI; i+=BLOCK_SIZE_I){
         for (int j=0; j< kJ; j+=BLOCK_SIZE_J){
           for (int k=0; k< kK; k+=BLOCK_SIZE_K){
             for (int i0=i; i0<i+BLOCK_SIZE_I; i0++){
               for (int j0=j; j0<j+BLOCK_SIZE_J; j0++){
-                temp = 0;
+                int temp = 0;
                 for (int k0=k; k0<k+BLOCK_SIZE_K; k0++){
                   //c[i0][j0] += a[i0][k0] * b[k0][j0];
                   temp += a[i0][k0] * b[k0][j0];
